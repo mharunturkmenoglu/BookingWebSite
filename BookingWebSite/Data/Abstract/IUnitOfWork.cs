18f50ea4b3b06ba6b997c457e7617a0f28ffd7a7
Mustafa Harun Türkmenoğlu <75381086+mharunturkmenoglu@users.noreply.github.com>
2022-08-29 11:37:25 +0300

    feat: add unitofwork

---
+++ BookingWebSite/Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace BookingWebSite.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IAppartmentRepository Appartments { get; }
+        IBookingRepository Bookings { get; }
+        ICompanyRepository Companies { get; }
+        IUserRepository Users { get; }
+        Task<int> SaveAsync();
+    }
+}
---
+++ BookingWebSite/Data/Concrete/EntityFramework/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using BookingWebSite.Data.Abstract;
+using BookingWebSite.Data.Concrete.EntityFramework.Contexts;
+using BookingWebSite.Data.Concrete.EntityFramework.Repositories;
+
+namespace BookingWebSite.Data.Concrete.EntityFramework
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Context _context;
+        private EfAppartmentRepository _appartmentRepository;
+        private EfBookingRepository _bookingRepository;
+        private EfCompanyRepository _companyRepository;
+        private EfUserRepository _userRepository;
+
+        public UnitOfWork(Context context)
+        {
+            _context = context;
+        }
+
+        public IAppartmentRepository Appartments => _appartmentRepository ?? new EfAppartmentRepository(_context);
+
+        public IBookingRepository Bookings => _bookingRepository ?? new EfBookingRepository(_context);
+
+        public ICompanyRepository Companies => _companyRepository ?? new EfCompanyRepository(_context);
+
+        public IUserRepository Users => _userRepository ?? new EfUserRepository(_context);
+
+        public async ValueTask DisposeAsync()
+        {
+            await _context.DisposeAsync();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
